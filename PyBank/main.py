953e2996139f7beef36b6fa17a817077bb3c427b
BijoyetaK <bijoyeta.euphoric@gmail.com>
2023-03-20 14:35:47 -0700

    Added comments in PyBank and PyPoll scripts

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,13 +1,15 @@
 import os
-import csv
 
+#Assigning the input csv file name, output text file, and folder for the files to variables and setting paths for them
 csvfile = 'budget_data.csv'
 datafolder = 'Resources'
 output_filename = 'analysis.txt'
 output_folder = 'analysis'
-#path to collect the data from the csv file in the csvfile folder
+#path to collect the data from the csv file in the csvfile folder, path to the textfile to write the output 
 budget_csv = '{0}/{1}/{2}'.format(os.getcwd(),datafolder,csvfile)
 output_filepath = '{0}/{1}/{2}'.format(os.getcwd(),output_folder,output_filename)
+
+#Initializing the lists for reading the input file data and also for perfoming individual calculations
 budgets = []
 budget_analysis =[]
 change_values = []
@@ -17,14 +19,15 @@ def getmonth_fromdate(datestring):
     month_name = datestring.split('-')[0]
     return month_name
 
+#reading the input file as lines 
 with open(budget_csv,'r') as csvfilehandle:
      budgetreader = csvfilehandle.readlines()
      i = 0
      for line in budgetreader:
-        if i > 0:
-            line = line.replace('\n','')
+        #skipping the Header record
+        if i > 0:           
+            line = line.replace('\n','') #replacing the newlines with blank
             line_array = line.split(',') #split the string from the list of strings by delimiter and put it into another list
-            budget_pf_int = int(line_array[1])
             budgets.append(line_array)
          
         i = i + 1
@@ -34,20 +37,27 @@ total_record_cnt = len(budgets) #total number of records
 i = 0
 netchange = 0
 netprofit = 0
-allmonths =[]
+allmonths =[] #empty list to contain just the months to get unique months later
 for budgetrow in budgets:
+
+    #storing current dates, profit/loss data and months 
     current_pf = int(budgetrow[1])
     current_dt = budgetrow[0]
     current_month = getmonth_fromdate(datestring=current_dt)
     allmonths.append(current_month)
+
+    #total profit/loss
     netprofit = netprofit + current_pf
-    if (i > 0) and (i < total_record_cnt - 1) and (i%2 != 0):
+
+    #calculating net changes only if it is having odds rows which are the period end dates. budget_analysis is a list of final calulated fields
+    #storing the changes of profit/loss in another list change_values and calculating the netprofit
+    if (i > 0) and (i < total_record_cnt - 1) and (i % 2 != 0): 
         open_pf_ls = int(budgets[i-1][1])
         change_in_pf_ls = current_pf - open_pf_ls
         netchange = netchange + change_in_pf_ls
         budget_analysis.append([current_dt,current_month,current_pf,open_pf_ls,change_in_pf_ls])
         change_values.append(change_in_pf_ls)
-    if (i == total_record_cnt -1) and (i%2 !=0):
+    if (i == total_record_cnt -1) and (i % 2 !=0): #for the last record
         open_pf_ls = int(budgets[i-1][1])
         change_in_pf_ls = current_pf - open_pf_ls
         netchange = netchange + change_in_pf_ls
@@ -55,19 +65,20 @@ for budgetrow in budgets:
         change_values.append(change_in_pf_ls) 
     i = i +1
 
-change_values.sort()
-avg_change = round((netchange * 1.0)/len(change_values),2)
+change_values.sort() #sorting the list of change values to calculate the greatest increase and decrease
+avg_change = round((netchange * 1.0)/len(change_values),2) #calculating average change
 gr_inc_change = change_values[-1]
 gr_dec_change = change_values[0]
-unique_months = list(set(allmonths))
+unique_months = list(set(allmonths)) #set function to fetch all unique months
 
+#fetching the corresponding dates on which greatest increase and decrease of net profit/loss took place
 for budgets in budget_analysis:
     if budgets[4] == gr_inc_change:
         date_of_gr_inc = budgets[0]
     if budgets[4] == gr_dec_change:
         date_of_gr_dec = budgets[0]
 
-
+#creating a message list to write all the calculations into output text file at once.
 messages = []
 messages.append('Financial Analysis\n')
 separator = "-" * 50 
@@ -81,6 +92,7 @@ messages.append('Avg change : ${0}\n'.format(str(avg_change)))
 messages.append("Greatest Increase in Profits: {0} (${1})\n" .format(str(date_of_gr_inc),str(gr_inc_change)))
 messages.append("Greatest Decrease in Profits: {0} (${1})\n" .format(str(date_of_gr_dec),str(gr_dec_change)))
 
+#prints to the terminal
 print('Total Dates: {0}'.format(str(total_record_cnt)))
 print('Total unique months: {0}'.format(str(len(unique_months))))
 print('Total profit/loss : ${0}'.format(str(netprofit)))
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,34 +1,43 @@
 import os
-import csv
 
+#Assigning the input csv file name, output text file, and folder for the files to variables and setting paths for them
 csvfile = 'election_data.csv'
 datafolder = 'Resources'
 output_filename = 'analysis.txt'
 output_folder = 'analysis'
-#path to collect the data from the csv file in the csvfile folder
+
+#path to collect the data from the csv file in the csvfile folder, path to the textfile to write the output 
 election_csv = '{0}/{1}/{2}'.format(os.getcwd(),datafolder,csvfile)
 output_filepath = '{0}/{1}/{2}'.format(os.getcwd(),output_folder,output_filename)
+
+#Using dictionaries to fetch the total votes for each candidate, key is the candidate name here 
+#reading and storing all the ballot data into another list for ballots. 
 ballots =[]
 candidates = {}
+
+#reading the input file as lines 
 with open(election_csv,'r') as csvfilehandle:
      csvreader = csvfilehandle.readlines()
      i = 0
      for line in csvreader:
+        #skipping the Header record and reading the file into line_array
         if i > 0:
             line = line.replace('\n','')
             line_array = line.split(',') #split the string from the list of strings by delimiter and put it into another list
-            ballots.append(line_array[0]) #number of votes in an array
-            candidate_name = line_array[2]
+            ballots.append(line_array[0]) #number of votes in ballots list
+            candidate_name = line_array[2] #preparing the candidate name data to store in candidates dictionaries. Key = Candidate name
             if candidate_name in candidates.keys():
+                #calculating the total ballot counts/votes for each candidate 
                 ballot_cnts = candidates[candidate_name]
                 ballot_cnts = ballot_cnts + 1
                 candidates[candidate_name] = ballot_cnts
             else:
+                #if candidate name is found as empty then initializing the dictionary
                 candidates[candidate_name]=1
          
         i = i + 1
 
-
+#creating a list candidate_votes to contain candidate, corresponding number of votes, and their percentage of votes compared to total no. of votes
 candidates_votes = []
 b_cnts = []
 for candidate in candidates.keys():
@@ -37,15 +46,22 @@ for candidate in candidates.keys():
     candidates_votes.append([candidate,candidate_ballot_cnt,candidate_ballot_percent])
     b_cnts.append(candidate_ballot_cnt)
 
+#sorting the total votes for each candidate in asceding order
 b_cnts.sort()
+
+#storing the winning and loosing candidates vote count 
 highest_ballot_cnt = b_cnts[-1]
 lowest_ballot_cnt = b_cnts[0]
+
+#to fetch the corresponding candidate name for the winner and loosing candidate from the candidates_votes list.
 for cd in candidates_votes:
     if cd[1] == highest_ballot_cnt:
         winning_candidate = cd[0]
     if cd[1] == lowest_ballot_cnt:
         loosing_candidate = cd[0]
 
+
+#creating a message list to write all the calculations into output text file at once.
 messages = []
 messages.append('\nElection Results\n')
 separator = "-" * 50 
@@ -58,7 +74,9 @@ for elements in candidates_votes:
 messages.append(separator)
 messages.append("Winner: {0}\n".format(winning_candidate))
 
+#writing the final output to the text file analysis.txt
 with open(output_filepath,'w') as outputfilehandler:
      outputfilehandler.writelines(messages)
 
+#Using join function to print all the items in the message list, adding the newline to have them printed in separate lines
 print('\n'.join(messages))
